77e2f18afa80ec11bb4e03052d6fc97b8418fa19
Nicolas Hedger <nicolas@hedger.ch>
2023-12-07 13:48:48 +0100

    reword comment

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -168,8 +168,7 @@ export class StatusBar {
 	}
 
 	public async checkForUpdates() {
-		// Only check for updates if we're using the bundled version and if an
-		// internet connection is available
+		// Only check for updates if we're using the bundled version
 		if (!this.usingBundledBiome) {
 			this.statusBarUpdateItem.hide();
 			return;
