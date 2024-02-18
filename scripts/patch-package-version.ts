dec2b1f05fdfc66509ced145e6e8a97ca9536d7b
Nicolas Hedger <nicolas@hedger.ch>
2024-02-18 09:21:28 +0100

    improve nightly version

--- scripts/patch-package-version.ts
+++ scripts/patch-package-version.ts
@@ -4,12 +4,10 @@ import { writeFileSync } from "fs";
  * Generates a nightly version identifier
  *
  * This helper function generates a nightly version identifier based on the
- * current date and time that looks like this: `{year}.{month}.{day}{hour}`.
+ * current date and time that looks like this: `{year}.{month}.{day}{hour}{min}`.
  *
  * For example, if the current date is 2024-02-17 and the current time is 23:00,
- * the generated version would be `2024.02.2723`.
- *
- * This means that at most, there can be one nightly build per hour.
+ * the generated version would be `2024.02.272300`.
  */
 const generateNightlyVersion = () => {
 	const today = new Date(
@@ -20,8 +18,9 @@ const generateNightlyVersion = () => {
 	const month = today.getMonth() + 1;
 	const day = today.getDate();
 	const hour = String(today.getHours()).padStart(2, "0");
+	const minute = String(today.getMinutes()).padStart(2, "0");
 
-	return `${year}.${month}.${day}${hour}`;
+	return `${year}.${month}.${day}${hour}${minute}`;
 };
 
 /**
