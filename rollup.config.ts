04ea6c42a2a7e623eb96ad3c989ca2b8844db078
Okinea Dev <hi@okinea.dev>
2025-01-29 09:08:53 +0200

    generate sourcemaps only in development and use rollup-plugin-delete instead of rimraf

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,11 @@
 import commonjs from "@rollup/plugin-commonjs";
+import del from "rollup-plugin-delete";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { defineConfig } from "rollup";
 import esbuild from "rollup-plugin-esbuild";
+
+const isDev = process.env.NODE_ENV === "development";
 
 export default defineConfig([
 	{
@@ -10,14 +13,19 @@ export default defineConfig([
 		output: {
 			dir: "out",
 			format: "cjs",
+			sourcemap: isDev,
 		},
 		plugins: [
+			del({
+				targets: "out/*",
+				runOnce: true,
+			}),
 			json(),
 			commonjs(),
 			nodeResolve(),
 			esbuild({
 				target: "node16",
-				minify: true,
+				minify: !isDev,
 			}),
 		],
 		external: ["vscode", "node:events"],
