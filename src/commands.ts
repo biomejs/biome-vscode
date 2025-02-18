15c63cfc2ba4ecc1b01b34195654518fd9249d95
Nicolas Hedger <nicolas@hedger.ch>
2025-02-18 18:18:14 +0100

    remove unwanted changes

--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,6 @@
 import { downloadBiome } from "./downloader";
 import { restart, start, stop } from "./lifecycle";
 import { info } from "./logger";
-import type { Project } from "./project";
-import type { Session } from "./session";
 import { state } from "./state";
 import { clearTemporaryBinaries } from "./utils";
 
@@ -46,23 +44,10 @@ export const downloadCommand = async () => {
 	await downloadBiome();
 };
 
-/**
- * Resets the Biome extension
- *
- * This command is exposed to the users as a command in the command palette.
- *
- * When calling this command, the extension will be stopped, all temporary
- * binaries will be removed, and the downloaded version of the Biome CLI will
- * be cleared from VS Code's global storage directory.
- */
 export const resetCommand = async () => {
 	await stop();
 	await clearTemporaryBinaries();
 	await state.context.globalState.update("downloadedVersion", undefined);
 	info("Biome extension was reset");
 	await start();
-};
-
-export const getAllProjects = (): Map<Project, Session> => {
-	return state.sessions;
 };
