a9a704601799cff73b7e992829cb69ef0d8e4e3e
Nicolas Hedger <nicolas@hedger.ch>
2026-03-15 10:32:56 +0100

    Revert "chore: migrate to rolldown (#977)"
    This reverts commit 2932328ba656468577d1d2a02b9e4b8728c8419c.

---
+++ rollup.config.ts
@@ -0,0 +1,39 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { defineConfig } from "rollup";
+import copy from "rollup-plugin-copy";
+import esbuild from "rollup-plugin-esbuild";
+
+export default defineConfig([
+	{
+		input: "src/main.ts",
+		output: {
+			dir: "out",
+			format: "cjs",
+			sourcemap: true,
+		},
+		plugins: [
+			json(),
+			commonjs(),
+			nodeResolve(),
+			esbuild({
+				target: "node16",
+				sourceMap: true,
+			}),
+			copy({
+				targets: [
+					{
+						src: "node_modules/web-tree-sitter/web-tree-sitter.wasm",
+						dest: "out",
+					},
+					{
+						src: "node_modules/tree-sitter-gritql/tree-sitter-gritql.wasm",
+						dest: "out",
+					},
+				],
+			}),
+		],
+		external: ["vscode", "node:events"],
+	},
+]);
